b4a28c909a7633fc43177cf6abeb6281f7ea9b43
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-03-05 19:39:25 -0500

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def memoize(function):
+    memo = {}
+
+    def helper(x):
+        if x not in memo:
+            memo[x] = function(x)
+        return memo[x]
+    return helper
+
+
+def rock_paper_scissors(n):
+    plays = ['rock', 'paper', 'scissors']
+    all_plays = []
+    pass
+
+
+rock_paper_scissors = memoize(rock_paper_scissors)
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
